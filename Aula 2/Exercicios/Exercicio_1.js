1bf559ded04c9df2d63671faaf72a1bd6155a1b0
outwake <larissa.mendy@gmail.com>
2026-01-27 14:01:38 -0300

    ADD FUNÇÃO NOME

--- Aula 2/Exercicios/Exercicio_1.js
+++ Aula 2/Exercicios/Exercicio_1.js
@@ -1,6 +1,7 @@
 
 const leia= require("readline-sync");
 
+let nome= leia.question("Digite o nome do funcionário:")
 let salario= leia.questionFloat("Qual é o salario:");
 let abono= leia.questionFloat("Qual é o abono:");
 
@@ -10,7 +11,7 @@ let novoSalario = Number(salario)+ Number(abono);
 //console.log ("O novo salario é ", novoSalario.toFixed(2))
 //toofixed fixar casas decimais
 
-console.log("Salario Liquido atualizado:", 
+console.log("O(a) funcionário(a)", nome,  " possui salario Liquido atualizado de:", 
     new Intl.NumberFormat('pt-BR',{
         style: 'currency',
         currency: 'BRL'
