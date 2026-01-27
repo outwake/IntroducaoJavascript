f47d1db3b848114b771d9ae3c8e394a5b5743245
outwake <larissa.mendy@gmail.com>
2026-01-27 13:55:46 -0300

    ADD FUNÇÃO NOME

--- Aula 2/Exercicios/Exercicio_2.js
+++ Aula 2/Exercicios/Exercicio_2.js
@@ -1,5 +1,6 @@
 const leia= require("readline-sync");
 
+let nome= leia.question("Digite o nome do aluno:")
 let nota1= leia.questionFloat("Digite a primeira nota ")
 let nota2= leia.questionFloat("Digite a segunda nota ")
 let nota3= leia.questionFloat("Digite a terceira nota ")
@@ -8,4 +9,4 @@ let nota4= leia.questionFloat("Digite a quarta nota ")
 let media=(Number(nota1)+Number(nota2)+Number(nota3)+Number(nota4))/4
 
 
-console.log("A média do aluno é:", media.toFixed(2))
+console.log(`O(a) aluno(a) ${nome} é: ${media.toFixed(2)}`)
